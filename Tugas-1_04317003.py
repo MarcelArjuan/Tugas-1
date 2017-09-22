2b5bc85669f3490a27e7fcf28b52aaf5768cd339
MarcelArjuan <32183516+MarcelArjuan@users.noreply.github.com>
2017-09-22 10:05:28 +0700

    Tugas-1

---
+++ Tugas-1_04317003.py
@@ -0,0 +1,10 @@
+nama = "Marcel Arjuan Setiawan"
+nim = "0317003"
+umur = 18
+print("Namaku :" + nama)
+print("Nim :" + nim)
+print("Umur")
+print(umur)
+print("Umur 5 Tahun ke Depan")
+print(umur + 5)
+
